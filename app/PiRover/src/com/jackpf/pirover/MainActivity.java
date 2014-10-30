de78ae25cf4cff7f7a1243ea0b6eebd471e11e47
jackpf <jack_farrelly@yahoo.co.uk>
2014-10-30 18:22:37 +0000

    Fixed unused warning

--- app/PiRover/src/com/jackpf/pirover/MainActivity.java
+++ app/PiRover/src/com/jackpf/pirover/MainActivity.java
@@ -3,7 +3,6 @@ package com.jackpf.pirover;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
-import android.net.wifi.WifiManager;
 import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
