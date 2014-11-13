a50b64dae2d0aba2f49e8e71dc9bba03aa83b878
jackpf <jack_farrelly@yahoo.co.uk>
2014-11-13 21:06:59 +0000

    Added video request test [#4]

--- app/PiRover/tests/src/com/jackpf/pirover/test/VideoTest.java
+++ app/PiRover/tests/src/com/jackpf/pirover/test/VideoTest.java
@@ -9,6 +9,8 @@ import android.test.ActivityInstrumentationTestCase2;
 import com.jackpf.pirover.MainActivity;
 import com.jackpf.pirover.Camera.BufferedVideo;
 import com.jackpf.pirover.Camera.Recorder;
+import com.jackpf.pirover.Model.RequestResponse;
+import com.jackpf.pirover.Request.PlaybackRequest;
 
 public class VideoTest extends ActivityInstrumentationTestCase2<MainActivity>
 {
@@ -52,5 +54,21 @@ public class VideoTest extends ActivityInstrumentationTestCase2<MainActivity>
         assertTrue(video.isLoaded());
         assertTrue(video.getFrameCount() > 0);
         assertNotNull(video.getFrame());
+    }
+    
+    public void testVideoRequest() throws Exception
+    {
+        String filename = Environment.getExternalStorageDirectory() + "/" + Recorder.RECORD_DIR + "/test.prr";
+        
+        if (!new File(filename).exists()) {
+            throw new Exception("Test file not present");
+        }
+        
+        BufferedVideo video = new BufferedVideo(filename);
+        
+        RequestResponse params = new PlaybackRequest(video).call();
+        
+        assertNotNull(params);
+        assertNotNull(params.get("drawable"));
     }
 }
