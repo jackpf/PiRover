c41b7df346d45031415644e101b9cb2178dff4fc
jackpf <jack_farrelly@yahoo.co.uk>
2014-11-25 22:14:12 +0000

    Lights [#35]

--- lights.cpp
+++ lights.cpp
@@ -1,22 +1,39 @@
 #include "lib/lib.hpp"
 #include "lib/gpio.hpp"
+#include "lib/network.hpp"
 
 #define LIGHT 11
 
 static struct argp_option options[] = {
-    {"blink", 'b', "BLINKSPEED", 0, "LED blink speed in microseconds"},
+    {"interval", 'i', "INTERVAL", 0, "Interval to check connection status"},
     {0}
 };
+
+bool connected = false;
 
 int main(int argc, char *argv[])
 {
     Lib::Args args(argc, argv, options, sizeof(options));
 
+    WLANStatus wlan;
+    int t = atoi(args.get("interval", "30"));
+
+    pthread_t thread;
+    pthread_create(&thread, NULL, blinkThread, NULL);
+
+    while (true) {
+        connected = wlan.isConnected();
+        sleep(t);
+    }
+}
+
+void blinkThread(void *data)
+{
     GPIO::setup();
 
     GPIO::pinMode(LIGHT, GPIO::OUT);
 
-    int t = atoi(args.get("blink", "1000000"));
+    int t = atoi(args.get("blink", ""));
 
     while (true) {
         GPIO::write(LIGHT, GPIO::HIGH);
