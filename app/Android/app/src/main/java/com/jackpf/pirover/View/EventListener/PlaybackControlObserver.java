64c616fb0b55069cb3055d6d2859a22d2612d9c8
jackpf <jack_farrelly@yahoo.co.uk>
2014-12-10 01:00:38 +0000

    Fixed wrong thread exception [#41]

--- app/Android/app/src/main/java/com/jackpf/pirover/View/EventListener/PlaybackControlObserver.java
+++ app/Android/app/src/main/java/com/jackpf/pirover/View/EventListener/PlaybackControlObserver.java
@@ -12,11 +12,13 @@ import java.util.Observer;
 
 public class PlaybackControlObserver implements Observer
 {
+    private PlaybackActivity activity;
     private ImageButton btnPlay;
     private ProgressBar pPlayback;
 
     public PlaybackControlObserver(PlaybackActivity activity)
     {
+        this.activity = activity;
         btnPlay = (ImageButton) activity.findViewById(R.id.play);
         pPlayback = (ProgressBar) activity.findViewById(R.id.video_progress);
     }
@@ -24,10 +26,16 @@ public class PlaybackControlObserver implements Observer
     @Override
     public void update(Observable observable, Object data)
     {
-        BufferedVideo video = (BufferedVideo) observable;
-        btnPlay.setSelected(!video.isPlaying());
+        final BufferedVideo video = (BufferedVideo) observable;
 
-        int progress = (int) Math.round(video.getFramePosition() * 100.0 / video.getFrameCount());
-        pPlayback.setProgress(progress);
+        activity.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                btnPlay.setSelected(!video.isPlaying());
+
+                int progress = (int) Math.round(video.getFramePosition() * 100.0 / video.getFrameCount());
+                pPlayback.setProgress(progress);
+            }
+        });
     }
 }
